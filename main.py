89dfd4ea88268180e641dad3f93f5fc5dea6724b
dan228-30 <145848543+dan228-30@users.noreply.github.com>
2024-04-25 11:22:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,42 +6,74 @@ import requests
 import os
 from dotenv import load_dotenv
 import pandas as pd
+import matplotlib
+
+matplotlib.use('Agg')  # Установить backend_agg
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 load_dotenv()
-
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 NUTRITIONIX_APP_ID = os.getenv("NUTRITIONIX_APP_ID")
 NUTRITIONIX_APP_KEY = os.getenv("NUTRITIONIX_APP_KEY")
 FOOD_EMOJIS = list(
-    "🍇🍈🍉🍊🍋🍋‍🟩🍌🍍🥭🍎🍏🍐🍑🍒🍓🫐🥝🍅🫒🥥🥑🍆🥔🥕🌽🌶️🫑🥒🥬🥦🧄🧅🥜🫘🌰🫚🫛🍄‍🟫🍞🥐🥖🫓🥨🥯🥞🧇🧀🍖🍗🥩🥓🍔🍟🍕🌭🥪🌮🌯🫔🥙🧆🥚🍳🥘🍲🫕🥣🥗🍿🧈🧂🥫🍝🍱🍘🍙🍚🍛🍜🍠🍢🍣🍤🍥🥮🍡🥟🥠🥡🍦🍧🍨🍩🍪🎂🍰🧁🥧🍫🍬🍭🍮🍯🍼🥛☕🫖🍵🍶🍾🍷🍸🍹🍺🍻🥂🥃🫗🥤🧋🧃🧉🥢🍽️🍴🥄🔪🫙🏺"
+    "🍇🍈🍉🍊🍋🍋‍🟩🍌🍍🥭🍎🍏🍐🍑🍒🍓🫐🥝🍅🫒🥥🥑🍆🥔🥕🌽🌶🫑🥒🥬🥦🧄🧅🥜🫘🌰🫚🫛🍄‍🟫🍞🥐🥖🫓🥨🥯🥞🧇🧀🍖🍗🥩🥓🍔🍟🍕🌭🥪🌮🌯🫔🥙🧆🥚🍳🥘🍲🫕🥣🥗🍿🧈🧂🥫🍝🍱🍘🍙🍚🍛🍜🍠🍢🍣🍤🍥🥮🍡🥟🥠🥡🍦🍧🍨🍩🍪🎂🍰🧁🥧🍫🍬🍭🍮🍯🍼🥛☕️🫖🍵🍶🍾🍷🍸🍹🍺🍻🥂🥃🫗🥤🧋🧃🧉🥢🍽🍴🥄🔪🫙🏺"
 )
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
+# Создаем или подключаемся к базе данных и создаем таблицу, если она не существует
 con = sql.connect("meals.db")
 c = con.cursor()
 c.execute("""CREATE TABLE IF NOT EXISTS meals
-             (id integer, time text, meal text, amount integer, calories integer)""")
+             (id integer, time text, meal text, amount integer, 
+              calories integer, protein, fat, carbs)""")
 con.commit()
 con.close()
 
 print("bot started")
 
 
-def get_nutrition_info(query):
+@bot.message_handler(commands=["start"])
+def start(message: telebot.types.Message):
+    bot.send_message(message.chat.id, "Привет! Я помогу тебе следить за питанием.\n"
+                                      "Введи /help для получения списка команд.")
+
+
+@bot.message_handler(commands=["help"])
+def help(message: telebot.types.Message):
+    bot.send_message(message.chat.id, """Список команд:
+/add_meal {блюдо} {граммы} - добавить блюдо
+/view_meals [day, week, month, year, all] - посмотреть список блюд за определенный период 
+/start - начать работу с ботом
+/help - список команд""")
+
+
+def get_nutrition_info(query, api_id=NUTRITIONIX_APP_ID, api_key=NUTRITIONIX_APP_KEY):
+    url = "https://trackapi.nutritionix.com/v2/natural/nutrients"
     headers = {
-        "x-app-id": NUTRITIONIX_APP_ID,
-        "x-app-key": NUTRITIONIX_APP_KEY,
+        "x-app-id": api_id,
+        "x-app-key": api_key,
+        "Content-Type": "application/json",
     }
-    payload = {"query": query}
-    response = requests.post(
-        "https://trackapi.nutritionix.com/v2/natural/nutrients",
-        headers=headers,
-        json=payload,
-    )
-    response.raise_for_status()
-    return response.json()
+    data = {"query": query}
+    response = requests.post(url, headers=headers, json=data)
+
+    if response.status_code == 200:
+        data = response.json()
+        foods = data["foods"]
+        if foods:
+            food = foods[0]
+            return {
+                "calories": food["nf_calories"],
+                "protein": food["nf_protein"],
+                "fat": food["nf_total_fat"],
+                "carbs": food["nf_total_carbohydrate"],
+            }
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+        return None
 
 
 @bot.message_handler(commands=["add_meal"])
@@ -51,42 +83,43 @@ def add_meal(message: telebot.types.Message):
         meal, amount = meal_info.rsplit(maxsplit=1)
         amount = int(amount)
 
-        data = get_nutrition_info(meal)
-        food_info = data["foods"][0]
-        calories_per_100g = (
-            int(food_info["nf_calories"])
-            / int(food_info.get("serving_weight_grams", 100))
-            * 100
-        )
-        total_calories = calories_per_100g * amount / 100
+        nutrition_info = get_nutrition_info(meal)
 
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        con = sql.connect("meals.db")
-        c = con.cursor()
-        c.execute(
-            "INSERT INTO meals VALUES (?, ?, ?, ?, ?)",
-            (message.from_user.id, current_time, meal, amount, total_calories),
-        )
+        if nutrition_info:
+            calories = nutrition_info["calories"]
+            protein = nutrition_info["protein"]
+            fat = nutrition_info["fat"]
+            carbs = nutrition_info["carbs"]
 
-        bot.send_message(
-            message.chat.id, f"{random.choice(FOOD_EMOJIS)} Блюдо успешно добавлено!"
-        )
+            # Рассчитываем общее количество КБЖУ
+            total_calories = calories * amount / 100
+            total_protein = protein * amount / 100
+            total_fat = fat * amount / 100
+            total_carbs = carbs * amount / 100
 
-        con.commit()
-        con.close()
-    except ValueError:
-        bot.send_message(
-            message.chat.id,
-            "❌ Неверный формат. Используй: /add_meal {блюдо} {граммы}.",
-        )
-    except requests.exceptions.RequestException:
-        bot.send_message(
-            message.chat.id,
-            "❌ Что-то пошло не так! Возможно, вы ввели еду не на английском, либо такой еды нет.",
-            parse_mode="Markdown",
-        )
-    except (KeyError, IndexError):
-        bot.send_message(message.chat.id, "❌ К сожалению такой еды нет у нас в базе!")
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            con = sql.connect("meals.db")
+            c = con.cursor()
+
+            c.execute(
+                "INSERT INTO meals (id, time, meal, amount, calories, protein, fat, carbs) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+                (
+                    message.from_user.id, current_time, meal, amount, total_calories, total_protein, total_fat,
+                    total_carbs)
+            )
+
+            con.commit()
+            con.close()
+
+            bot.send_message(
+                message.chat.id, f"{random.choice(FOOD_EMOJIS)} Блюдо успешно добавлено!"
+            )
+        else:
+            bot.send_message(message.chat.id, "Информация о КБЖУ не найдена.")
+
+    except Exception as e:
+        print(f"Ошибка: {e}")
+        bot.send_message(message.chat.id, "Произошла ошибка. Попробуйте еще раз.")
 
 
 @bot.message_handler(commands=["view_meals"])
@@ -95,95 +128,90 @@ def view_meals(message: telebot.types.Message):
         period = "day"
     else:
         _, period = message.text.split(maxsplit=1)
+
     if period == "day":
         today = datetime.now().strftime("%Y-%m-%d")
 
         con = sql.connect("meals.db")
         c = con.cursor()
         c.execute(
-            "SELECT meal, amount, calories FROM meals WHERE time LIKE ? AND id = ?",
+            "SELECT meal, amount, calories, protein, fat, carbs FROM meals WHERE time LIKE ? AND id = ?",
             (f"{today}%", f"{message.from_user.id}"),
         )
         meals = c.fetchall()
-        con.commit()
         con.close()
 
         if meals:
-            calories_total = [cal for _, _, cal in meals]
+            calories_total, protein_total, fat_total, carbs_total = 0, 0, 0, 0
             meal_list = "\n".join(
                 [
-                    f"- {meal} ({amount}g): *{calories:.2f} kcal*"
-                    for meal, amount, calories in meals
+                    f"- {meal} ({amount}g): *{calories:.2f} kcal, {protein:.2f}g P, {fat:.2f}g F, {carbs:.2f}g C*"
+                    for meal, amount, calories, protein, fat, carbs in meals
                 ]
             )
+            for _, _, calories, protein, fat, carbs in meals:
+                calories_total += calories
+                protein_total += protein
+                fat_total += fat
+                carbs_total += carbs
+
             bot.send_message(
                 message.chat.id,
-                f"{random.choice(FOOD_EMOJIS)} *Вот ваши сегодняшние блюда*:\n\n{meal_list}\n\n📝 В сумме: *{sum(calories_total):.2f} kcal* за день",
+                f"""{random.choice(FOOD_EMOJIS)} *Вот ваши сегодняшние блюда*:\n\n{meal_list}\n\n📝 В сумме: *{calories_total:.2f} kcal, {protein_total:.2f}g P, {fat_total:.2f}g F, {carbs_total:.2f}g C* за день""",
                 parse_mode="Markdown",
             )
         else:
             bot.send_message(
                 message.chat.id, "❌ Вы пока что не добавляли никакой еды за сегодня!"
             )
-    elif period in ("week", "month", "year"):
-        try:
-            if period == "week":
-                start_date = (datetime.now() - timedelta(days=7)).strftime("%Y-%m-%d")
-            elif period == "month":
-                start_date = (datetime.now() - timedelta(days=30)).strftime("%Y-%m-%d")
-            elif period == "year":
-                start_date = (datetime.now() - timedelta(days=365)).strftime("%Y-%m-%d")
 
-            con = sql.connect("meals.db")
-            c = con.cursor()
-            data = c.execute(
-                "SELECT calories, strftime('%Y-%m-%d', time) as date FROM meals WHERE time >= ? AND id = ?",
-                (start_date, f"{message.from_user.id}"),
-            )
-            calories_by_date = {}
-            for calories, date in data.fetchall():
-                calories_by_date.setdefault(date, []).append(calories)
-            con.commit()
-            con.close()
 
-            total_days = len(calories_by_date)
-            avg_calories = (
-                sum(sum(daily_calories) for daily_calories in calories_by_date.values())
-                / total_days
-            )
+@bot.message_handler(commands=["graph"])
+def send_calories_graph(message: telebot.types.Message):
+    user_id = message.from_user.id
+    period = "week"  # По умолчанию строим график за неделю
 
-            bot.send_message(
-                message.chat.id,
-                f"""{random.choice(FOOD_EMOJIS)} Средий показатель килокалорий за {f'''последний {"месяц" if period == "month" else "год"}''' if period != "week" else "последнюю неделю"}:\n\n*{avg_calories:.2f} kcal* в день.""",
-                parse_mode="Markdown",
-            )
-
-        except ZeroDivisionError:
-            bot.send_message(message.chat.id, "❌ Не найдено блюд за этот период.")
-    elif period == "all":
+    try:
         con = sql.connect("meals.db")
-        c = con.cursor()
-        meals = c.execute(
-            "SELECT time, meal, amount, calories FROM meals WHERE id = ?",
-            (f"{message.from_user.id}",),
-        ).fetchall()
-        con.commit()
+        df = pd.read_sql_query(f"SELECT time, calories FROM meals WHERE id = {user_id}", con)
         con.close()
 
-        if meals:
-            df = pd.DataFrame(meals, columns=["Time", "Meal", "Grams", "Kcalories"])
-            file_path = "meals_data.csv"
-            df.to_csv(file_path, index=False)
-            with open(file_path, "rb") as f:
-                bot.send_document(message.chat.id, f)
-            os.remove(file_path)
-        else:
-            bot.send_message(message.chat.id, "❌ Никаких данных по еде не найдено.")
-    else:
-        bot.send_message(
-            message.chat.id,
-            "❌ Неверный период. Используй: day, week, month, year, или all",
-        )
+        df['time'] = pd.to_datetime(df['time'])
+
+        df_daily = df.groupby(df['time'])['calories'].sum().reset_index()
+
+        if period == "week":
+            start_date = (datetime.now() - timedelta(days=6)).date()
+            df_daily = df_daily[df_daily['time'].dt.date >= start_date]  # Добавить .dt.date
+        elif period == "month":
+            start_date = datetime.now() - timedelta(days=29)
+            df_daily = df_daily[df_daily['time'].dt.date >= start_date]  # Добавить .dt.date
+
+        # Строим график
+        plt.figure(figsize=(10, 5))
+        plt.plot(df_daily['time'], df_daily['calories'])
+        plt.xlabel("Дата")
+        plt.ylabel("Калории (ккал)")
+        plt.title("Потребление калорий")
+        plt.grid(True)
+
+        # Формат даты - день-месяц
+        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d-%m'))
+
+        # Сохраняем график в файл
+        plot_path = "plot.png"
+        plt.savefig(plot_path)
+
+        # Отправляем график пользователю
+        with open(plot_path, "rb") as f:
+            bot.send_photo(message.chat.id, f)
+
+        # Удаляем файл с графиком
+        os.remove(plot_path)
+
+    except Exception as e:
+        print(f"Ошибка: {e}")
+        bot.send_message(message.chat.id, "❌ Произошла ошибка при построении графика.")
 
 
 bot.infinity_polling()
